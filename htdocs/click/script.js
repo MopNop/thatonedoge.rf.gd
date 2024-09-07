4f4266a3c632ff292eb2ef399c9a6a9b109350fd
Xarrak-Dev <xarrak99@gmail.com>
2024-09-07 15:50:48 -0700

    Update script.js

--- htdocs/click/script.js
+++ htdocs/click/script.js
@@ -10,6 +10,9 @@ const exclickpricemult = 1.2; //extra click price multiplier
 const basecpscost = 50; //base cost of click per second
 const cpsclickpricemult = 1.5; //click per second price multiplier
 
+var exclickcost = 10;
+var cpsclickcost = 50;
+
 //misc vars
 var intervalID = window.setInterval(cpsTick, 100); //sets a variable for some random shit man idk
 
@@ -27,12 +30,13 @@ window.onload = loaded();
 //when the page is loaded
 function loaded() {
 	updateLabels(); //update the labels
+	updateCosts(); //update the costs
 }
 
 //every tenth of a second
 function cpsTick() {
 	clicks += (cps - 1) / 10; //add a tenth of cps to clicks
-	updateLabels();
+	updateLabels(); //update the labels
 }
 
 //when main button clicked
@@ -47,6 +51,7 @@ function exclick() {
 	if (clicks >= exclickcost) {
 		clicks -= exclickcost; //subtract the cost
 		++cpp; //add 1 to the cpp
+		updateCosts(); //update the costs
 		updateLabels(); //update the labels
 	}  
 }
@@ -57,15 +62,19 @@ function cpsclick() {
 	if (clicks >= cpsclickcost) {
 		clicks -= cpsclickcost; //subtract the cost
 		++cps; //add 1 to the cps
+		updateCosts(); //update the costs
 		updateLabels(); //update the labels
 	}
 }
 
-//update labels
-function updateLabels() {
+//update costs
+function updateCosts() {
 	exclickcost = Math.round(baseexclickcost * (exclickpricemult ** (cpp + 1))); //update the price of cpp
 	cpsclickcost = Math.round(basecpscost * (cpsclickbuttonmult ** (cps + 1))); //update the price of cps
-	
+}
+
+//update labels
+function updateLabels() {
 	cpptracker.textContent = cpp + " Clicks per Press"; //update the clicks per press label
 	counter.textContent = Math.round(clicks) + " Clicks"; //update the clicks label
 	cpstracker.textContent = cps + " Clicks per Second"; //update the clicks per second label
